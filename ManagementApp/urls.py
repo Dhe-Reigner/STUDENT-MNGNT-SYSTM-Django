98395c54facac28c383e6b43f33331bd64fe4f5f
Dhe-Reigner <martinkagema01@gmail.com>
2024-11-13 11:20:28 +0300

    Initial commit

--- ManagementApp/urls.py
+++ ManagementApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home,name='my_home'),
+    path('', views.home,name='index'),
+    path('<int:id>', views.view_student, name='view_student'),
 ]
--- ManagementApp/views.py
+++ ManagementApp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . models import Student
 
 def home(request):
     return render(request, 'students/index.html',{
         'students':Student.objects.all()
     })
+def view_student(request, id):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse("index"))
