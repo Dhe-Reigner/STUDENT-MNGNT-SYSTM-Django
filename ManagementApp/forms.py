895455c87cc1de2d7e1e422743b46b773fd972f0
Dhe-Reigner <martinkagema01@gmail.com>
2024-11-14 07:55:34 +0300

    Initial commit

---
+++ ManagementApp/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Student
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['student_number', 'first_name', 'last_name', 'email', 'field_of_study', 'mean_score']
+        labels = {
+            'student_number': 'Student Number',
+            'first_name' : 'First Name',
+            'last_name' : 'Last Name',
+            'email' : 'Email',
+            'field_of_study' : 'Field of Study',
+            'mean_score' : 'Mean Score'
+        }
+        widgets = {
+        'student_number': forms.NumberInput(attrs={'class':'form-control'}),
+        'first_name': forms.TextInput(attrs={'class':'form-control'}),
+        'last_name' :forms.TextInput(attrs={'class':'form-control'}),
+        'email' :forms.TextInput(attrs={'class':'form-control'}),
+        'field_of_study' :forms.EmailField(attrs={'class':'form-control'}),
+        'mean_score' : forms.NumberInput(attrs={'class':'form-control'}),
+            
+    }    
--- ManagementApp/views.py
+++ ManagementApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
 from . models import Student
+from .forms import StudentForm
 
 def home(request):
     return render(request, 'students/index.html',{
@@ -10,3 +12,34 @@ def home(request):
 def view_student(request, id):
     student = Student.objects.get(pk=id)
     return HttpResponseRedirect(reverse("index"))
+
+def add(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            new_student_number = form.cleaned_data['student_number']
+            new_first_name = form.cleaned_data['first_name']
+            new_last_name = form.cleaned_data['last_name']
+            new_email = form.cleaned_data['email']
+            new_field_of_study = form.cleaned_data['field_of_study']
+            new_mean_score = form.cleaned_data['mean_score']
+            
+            new_student = Student(
+                student_number = new_student_number,
+                first_name = new_first_name,
+                last_name = new_last_name,
+                email = new_email,
+                field_of_study = new_field_of_study,
+                mean_score = new_mean_score
+            )
+            new_student.save()
+            return render(request, 'student/add.html',{
+                'form':StudentForm(),
+                'success': True
+            })
+    else:
+        form = StudentForm()
+        return render(request, 'students/add.html',{
+            'form': StudentForm()
+        })        
+        
